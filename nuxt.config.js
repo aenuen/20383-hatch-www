16d644f4ec2acbde4d0fc1a3bf2bd1d6876ea998
aenuen <aenuen@qq.com>
2024-08-05 20:54:21 +0800

    优化：合并CSS

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,5 +23,9 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
     transpile: [/^element-ui/],
-  },
+    // 合并CSS
+    extractCSS: true, optimization: {
+      splitChunks: { cacheGroups: { styles: { name: 'styles', test: /\.(css|vue)$/, chunks: 'all', enforce: true } } }
+    }
+  }
 }
